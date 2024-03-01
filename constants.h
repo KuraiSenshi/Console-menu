e4683626c73fbd4a505472fc7fce55a6ee616edb
Reginald Raymond <cattory3@gmail.com>
2024-03-01 18:05:32 +0500

    added .h file containing constants

---
+++ constants.h
@@ -0,0 +1,13 @@
+#pragma once
+
+namespace buttons {
+	inline constexpr char UP{ 72 };
+	inline constexpr char DOWN{ 80 };
+	inline constexpr char ESC{ 27 };
+	inline constexpr char ENTER{ 13 };
+}
+
+namespace coordinates {
+	inline constexpr int X{ 50 };
+	inline constexpr int Y{ 13 };
+}
